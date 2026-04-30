0278dec5f3967cb432a4d5ae03d8e9113ce4b08a
imanzzz-zz <itzemmanuel.aquino.ed@gmail.com>
2026-04-30 23:16:06 +0800

    Update Product.cs

--- ShoppingCartActivity/Product.cs
+++ ShoppingCartActivity/Product.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCartActivity
+{
+    internal class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+        public string Category;
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id}. Product: {Name} | Category: {Category} | Price: {Price} | Stock: {RemainingStock}");
+        }
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+        public bool SufficientStock(int quantity)
+        {
+            return RemainingStock >= quantity;
+        }
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+    }
+}
 
