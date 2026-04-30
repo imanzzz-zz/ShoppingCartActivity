c690b84db0264be76c0ceb9da88d7f31668164e6
imanzzz-zz <itzemmanuel.aquino.ed@gmail.com>
2026-05-01 00:16:35 +0800

    Update Program.cs

--- ShoppingCartActivity/Program.cs
+++ ShoppingCartActivity/Program.cs
@@ -1,5 +1,29 @@
 using System;
 
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. Product: {Name} | Price: {Price} | Stock: {RemainingStock}");
+    }
+    public double GetItemTotal(int quantity)
+    { 
+        return Price * quantity;
+    }
+    public bool SufficientStock(int quantity)
+    {
+        return RemainingStock >= quantity;
+    }
+    public void DeductStock(int quantity)
+    {
+        RemainingStock -=  quantity;
+    }
+}
+
 class Cart
 {
     public Product product;
